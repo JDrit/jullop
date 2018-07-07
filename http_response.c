668fdb87e6726274aac056f692f7a27793d01c8d
Joseph Batchik <josephbatchik@gmail.com>
2018-07-07 14:51:49 -0700

    added more response codes

--- http_response.c
+++ http_response.c
@@ -9,14 +9,30 @@
 
 static inline char *get_reason(int status_code) {
   switch (status_code) {
+
+  // 1xx: Informational - Request received, continuing process
   case 100: return "Continue";
   case 101: return "Switching Protocols";
+
+  // 2xx: Success - The action was successfully received, understood, and accepted
   case 200: return "Ok";
   case 201: return "Created";
   case 202: return "Accepted";
   case 203: return "Non-Authoritative Information";
   case 204: return "No Content";
   case 205: return "Reset Content";
+  case 206: return "Partial Content";
+
+  // 3xx: Redirection - Further action must be taken in order to complete the request
+  case 300: return "Multiple Choices";
+  case 301: return "Moved Permanently";
+  case 302: return "Found";
+  case 303: return "See Other";
+  case 304: return "Not Modified";
+  case 305: return "Use Proxy";
+  case 307: return "Temporary Redirect";
+
+  // 4xx: Client Error - The request contains bad syntax or cannot be fulfilled
   case 400: return "Bad  Request";
   case 401: return "Unauthorized";
   case 402: return "Payment Required";
@@ -24,7 +40,18 @@ static inline char *get_reason(int status_code) {
   case 404: return "Not Found";
   case 405: return "Method Not Allowed";
   case 406: return "Not Acceptable";
+  case 407: return "Proxy Authentication Required";
+  case 408: return "Request Time-out";
+  case 409: return "Conflict";
+  case 410: return "Gone";
+
+  // 5xx: Server Error - The server failed to fulfill an apparently valid request  
   case 500: return "Internal Server Error";
+  case 501: return "Not Implemented";
+  case 502: return "Bad Gateway";
+  case 503: return "Service Unavailable";
+  case 504: return "Gateway Time-out";
+  case 505: return "HTTP Version not supported";
   default: return "Other";
   }      
 }
@@ -67,6 +94,6 @@ char *init_http_response(int status_code, char *body, size_t body_len) {
 	  status_code, reason_phrase,
 	  body_len,
 	  (int) body_len, body);
+  
   return buf;
 }
-			 
